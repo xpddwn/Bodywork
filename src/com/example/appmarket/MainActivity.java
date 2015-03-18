847c2f4665342d8cccb617d7abad05173b9c1459
yoyo <yoyo9170@163.com>
2015-03-18 13:22:18 +0800

    remove the title bar

--- src/com/example/appmarket/MainActivity.java
+++ src/com/example/appmarket/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,9 +12,9 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
