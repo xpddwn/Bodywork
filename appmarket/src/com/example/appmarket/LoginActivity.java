9dbe933568977335c9ac8e638b7de10ace9d0323
yoyo <yoyo9170@163.com>
2015-03-25 15:34:28 +0800

    yoyo commit for finish the user_register view

--- appmarket/src/com/example/appmarket/LoginActivity.java
+++ appmarket/src/com/example/appmarket/LoginActivity.java
@@ -1,15 +1,20 @@
 package com.example.appmarket;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 
 public class LoginActivity extends Activity {
 	private ImageButton back;
+	private TextView register;
+	
+	private Intent RegisterIntent;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,17 @@ public class LoginActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				LoginActivity.this.finish();
+			}
+		});
+		
+		register = (TextView)findViewById(R.id.user_register);
+		register.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				RegisterIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+				startActivity(RegisterIntent);
 			}
 		});
 	}
---
+++ appmarket/src/com/example/appmarket/RegisterActivity.java
@@ -0,0 +1,30 @@
+package com.example.appmarket;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+
+public class RegisterActivity extends Activity{
+	private ImageButton back;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.register);
+		
+		back = (ImageButton)findViewById(R.id.back);
+		back.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				RegisterActivity.this.finish();
+			}
+		});
+	}
+
+}
