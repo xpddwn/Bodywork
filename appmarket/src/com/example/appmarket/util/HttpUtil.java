f7509ca43e20117c9da640919ae3fdc3f62b5e4c
yoyo <yoyo9170@163.com>
2015-03-30 21:12:21 +0800

    add the android-async-http lib and finish the register function

---
+++ appmarket/src/com/example/appmarket/util/HttpUtil.java
@@ -0,0 +1,50 @@
+package com.example.appmarket.util;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.BinaryHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+public class HttpUtil {
+	 private static     AsyncHttpClient client =new AsyncHttpClient();   
+	    private static String baseurl = "http://123.57.133.126/app/v1/";
+	    
+	    static
+	    {
+	        client.setTimeout(10000);  
+	    }
+	    public static void get(RequestParams params,AsyncHttpResponseHandler res)  
+	    {
+	        client.get(baseurl, params,res);
+	    }
+	    public static void get(String urlString,RequestParams params,JsonHttpResponseHandler res)   
+	    {
+	        client.get(urlString, params,res);
+	    }
+	    public static void get(String uString, BinaryHttpResponseHandler bHandler)   
+	    {
+	        client.get(uString, bHandler);
+	    }
+	    public static AsyncHttpClient getClient()
+	    {
+	        return client;
+	    }
+	    public static void post(String tag,RequestParams params,JsonHttpResponseHandler res) 
+	    {
+	    	String httpurl = null;
+	    	if(tag.equalsIgnoreCase("register"))
+	    	{
+	    		httpurl = baseurl + "register";
+	    	}else if(tag.equalsIgnoreCase("login"))
+	    	{
+	    		httpurl = baseurl + "login";
+	    	}
+	    	client.post(httpurl, params, res);
+	    }
+		public static void post(RequestParams params,
+				AsyncHttpResponseHandler res) {
+			// TODO Auto-generated method stub
+			client.post(baseurl, params, res);
+		}
+}
